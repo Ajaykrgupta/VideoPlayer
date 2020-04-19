a65c8d40f0a9109d8abd3e8b259e93288b9ca1df
Ajay kumar <ajay.krbca@gmail.com>
2020-04-19 18:36:07 +0530

    Add files

---
+++ src/app/safe.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Pipe({
+  name: 'safe'
+})
+export class SafePipe implements PipeTransform {
+
+  constructor(private sanitizer: DomSanitizer){}
+
+  transform(url:any){
+    if(url){
+      url = url.replace('watch?v=','embed/');
+      }
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url+'?autoplay=1');
+  }
+
+}
